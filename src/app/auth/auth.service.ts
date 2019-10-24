822f0de49ffad8cea99eafa44ad381ab4dc34856
Chad Saglam <saglam.chd@gmail.com>
2019-10-24 06:06:46 +0200

    Sending Login Requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 
@@ -43,5 +44,17 @@ export class AuthService {
           return throwError(errorMessage);
         })
       );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAEfu2AhZfVcCINkCIhSUfpwofCCWgn3EI' ,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
+        }
+      )
   }
 }
